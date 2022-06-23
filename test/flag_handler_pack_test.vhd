276f24aa66fe79fd41df54ebc39731a9033cfa88
ArthurD <arthur.docquois@wanadoo.fr>
2022-06-23 22:28:14 +0200

    Correction of syntax errors in tests

--- test/flag_handler_pack_test.vhd
+++ test/flag_handler_pack_test.vhd
@@ -1,67 +1,40 @@
+----------------------------------------------------------------------------------
+-- TUM VHDL Assignment
+-- Arthur Docquois, Maelys Chevrier, Timothée Carel, Roman Canals
+--
+-- Create Date: 06/06/2022
+-- Project Name: CPU Functional model
+
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 library work;
 use work.flag_handler_pack.all;
+use work.cpu_defs_pack.all;
 
 
 entity Test is
 end Test;
-    
+
 architecture flag_handler_pack_test of Test is
     begin
         process
+        variable Zero, Negative, Overflow: bit := '0';
+        constant Data:  data_type := "111111111111";
+        begin
+        Set_Flags_Load(Data, Zero, Negative, Overflow);
 
-        variable Zero, Negative, Overflow: inout bit := '0', '0', '0';
-        constant Data: in data_type := '111111111111';
+        assert Zero = '0' and Negative = '1' and Overflow = '0';
 
-        Set_Flags_Load(Data, (Zero, Negative, Overflow))
-
-        assert Zero == '1' and Negative == '1' and Overflow == 0
-
-        Zero, Negative, Overflow <= '0', '0', '0';
-        Set_Flags_Logic(Data, (Zero, Negative, Overflow))
-
-        assert Zero == '1' and Negative == '1' and Overflow == '0'
-
-
-    --     procedure Set_Flags_Load(
-    --     constant Data: in data_type;
-    --     variable Zero,Negative,Overflow : inout bit) is
-    --         variable C_TMP : bit := '0';
-    --         variable N_TMP : bit;
-    --         variable R_TMP : data_type;
-    --         variable T : integer range 0 to 3;
-    --         constant zero_v: data_type := (others => '0');
-    --     begin
-    --         for i in Data'reverse_range loop``
-    --             T := bit'pos(Data(i))+ bit'pos(C_TMP);
-    --             R_TMP(i) := bit'val(T mod 2);
-    --             C_TMP := bit'val(T / 2);
-    --         end loop;
-            
-    --         T := bit'pos(Data(Data'length-1))+bit'pos(C_TMP);
-    --         N_TMP := bit'val(T mod 2);
-    --         Overflow := R_TMP( data_width-1 ) XOR N_TMP;
-    --         Negative := N_TMP;
-            
-    --         Zero := bit'val(boolean'pos(R_TMP = zero_v));
-    -- end Set_Flags_Load;
-
-    end process
-end architecture
-
--- 100
--- T = 0
--- R_T = '0'
--- C_T = '0'
-
--- T = 0
--- R_T = '00'
--- C_T = '0'
-
--- T = 1
--- R_T = '100'
--- C_T = '0'
-
--- T = 0
--- N_T = 0
-
--- 000
+    end process;
+end architecture;
--- test/logicArithm_defs_pack_test.vhd
+++ test/logicArithm_defs_pack_test.vhd
@@ -1,3 +1,23 @@
+----------------------------------------------------------------------------------
+-- TUM VHDL Assignment
+-- Arthur Docquois, Maelys Chevrier, Timothée Carel, Roman Canals
+--
+-- Create Date: 06/06/2022
+-- Project Name: CPU Functional model
+
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 library work;
 use work.logicArithm_defs_pack.all;
 
@@ -8,16 +28,16 @@ end Test;
 architecture logicArithm_defs_pack_test of Test is
 begin
     process
-        constant test_vector: bit_vector := '11110000'
-        
-        test_vector <= "NOT"(test_vector)
-        assert test_vector == '00001111'
+        variable test_vector: bit_vector := "11110000";
+        begin
+        test_vector := "NOT"(test_vector);
+        assert test_vector = "00001111";
 
-        test_vector <= test_vector "AND" '11000011'
-        assert test_vector == '00000011'
-        
-        test_vector <= INC(test_vector)
-        assert test_vector == '00000100'
-        
-    end process
-end architecture
+        test_vector := test_vector AND "11000011";
+        assert test_vector = "00000011";
+
+        test_vector := INC(test_vector);
+        assert test_vector = "00000100";
+
+    end process;
+end architecture;
