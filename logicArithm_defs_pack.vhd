c4903c3cb4af199cac2ae345bcc34603f7216a21
Maelys <maelys.chevrier@imt-atlantique.net>
2022-06-23 14:06:48 +0200

    subc, SLL,SRA, SRL, SLT and SLTU done

--- logicArithm_defs_pack.vhd
+++ logicArithm_defs_pack.vhd
@@ -33,12 +33,39 @@ package logicArithm_defs_pack is
         variable R : out data_type;
         variable Z,CO,N,O : out bit);
 
+    procedure EXEC_SUBC (
+        constant A,B : in data_type;
+        constant CI : in bit;
+        variable R : out data_type;
+        variable Z,CO,N,O : out bit);
+
+    function "SLL" (constant A :data_type)
+        return data_type;
+
+    function "SRL" (constant A :data_type)
+        return data_type;
+
+    function "SRA" (constant A :data_type)
+        return data_type;
+
     function "NOT" (constant A :data_type)
         return data_type;
 
     function "AND" (constant A, B :data_type)
         return data_type;
     
+    function "OR" (constant A, B :data_type)
+        return data_type;
+
+    function "XOR" (constant A, B :data_type)
+        return data_type;
+
+    function "SLT" (constant A, B :data_type)
+        return data_type;
+
+    function "SLTU" (constant A, B :data_type)
+        return data_type;
+
 
 
 end logicArithm_defs_pack;
@@ -56,9 +83,9 @@ package body logicArithm_defs_pack is
                 C := A(i) and C;
             end loop;
             return R;
-     end INC;
+    end INC;
 
-     procedure EXEC_ADDC (
+    procedure EXEC_ADDC (
         constant A,B : in data_type;
         constant CI : in bit;
         variable R : out data_type;
@@ -84,6 +111,50 @@ package body logicArithm_defs_pack is
             Z := bit'val(boolean'pos(R_TMP = zero_v));
     end EXEC_ADDC;
 
+    procedure EXEC_SUBC (
+        constant A,B : in data_type;
+        constant CI : in bit;
+        variable R : out data_type;
+        variable Z,CO,N,O : out bit ) is
+        variable C_TMP : bit := CI;
+        variable N_TMP : bit;
+        variable R_TMP : data_type;
+        variable T : integer range 0 to 3;
+        constant zero_v: data_type := (others => '0');
+        begin
+            for i in A'reverse_range loop
+                T := bit'pos(A(i))- bit'pos(B(i))- bit'pos(C_TMP);
+                R_TMP(i) := bit'val(T mod 2);
+                C_TMP := bit'val(T / 2);
+            end loop;
+            CO := C_TMP;
+            T := bit'pos(A(A'length-1))+ bit'pos(B(B'length-1))+
+            bit'pos(C_TMP);
+            N_TMP := bit'val(T mod 2);
+            O := R_TMP( data_width-1 ) XOR N_TMP;
+            N := N_TMP;
+            R := R_TMP;
+            Z := bit'val(boolean'pos(R_TMP = zero_v));
+    end EXEC_SUBC;
+
+    function "SLL" (constant A : data_type)
+        return data_type is
+    begin
+        return  A(10 down to 0)& '0' ;
+    end "SLL";
+
+    function "SRL" (constant A : data_type)
+        return data_type is
+    begin
+        return '0' & A(11 down to 1) ;
+    end "SRL";
+
+    function "SRA" (constant A : data_type)
+        return data_type is
+    begin
+        return A(11 down to 11)&'0' & A(10 down to 1) ;
+    end "SRA";
+
     function "NOT" (constant A : data_type)
         return data_type is
     begin
@@ -99,27 +170,25 @@ package body logicArithm_defs_pack is
     function "XOR" (constant A,B : data_type)
         return data_type is
     begin
-        return bit_vector2natural(natural2bit_vector( A , data_width ) XOR natural2bit_vector( B , data_width ) );
+        return A XOR B;
     end "XOR"
 
     function "OR" (constant A,B : data_type)
         return data_type is
     begin
-        return bit_vector2natural(natural2bit_vector( A , data_width ) OR natural2bit_vector( B , data_width ) );
+        return A OR B;
     end "OR"
-    
-    function "SUBC" (constant A,B : data_type)
-        return data_type is
-    begin
-        return bit_vector2natural(natural2bit_vector( A , data_width ) SUBC natural2bit_vector( B , data_width ) );
-    end "SUBC"
 
-    function "SUB" (constant A,B : data_type)
+    function "SLT" (constant A, B :data_type)
         return data_type is
     begin
-        return bit_vector2natural(natural2bit_vector( A , data_width ) SUB natural2bit_vector( B , data_width ) );
-    end "SUB"
+        return A < B;
+    end "SLT"
+
+    function "SLTU" (constant A, B :data_type)
+        return data_type is
+    begin
+        return A < unsigned(B);
+    end "SLTU"
 
 end logicArithm_defs_pack;
-
-
